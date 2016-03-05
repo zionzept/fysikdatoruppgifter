6498f5172b0b01c5279d6fb9aae68036169c48a5
zionzept <zionzept@gmail.com>
2016-03-05 17:27:33 +0100

    iiiiiiiiiiiii

--- src/diagram/Diagram.java
+++ src/diagram/Diagram.java
@@ -1,25 +1,46 @@
 package diagram;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
 public class Diagram {
 
-	private static String xcaption;
-	private static String ycaption;
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 400;
 	
-	private static LinkedList<Plot> plots;
+	private String xcaption;
+	private String ycaption;
+	
+	private LinkedList<Plot> plots;
+	
+	private BufferedImage image;
+	private boolean imageExpired;
 	
 	public Diagram(String xcaption, String ycaption) {
 		this.xcaption = xcaption;
 		this.ycaption = ycaption;
+		imageExpired = true;
 	}
 	
 	public void addPlot(Plot plot) {
 		plots.add(plot);
+		imageExpired = false;
 	}
 	
-	public Image createImage() {
-		return null;
+	public Image getImage() {
+		if (imageExpired) {
+			generateImage();
+		}
+		return image;
+	}
+	
+	public Image generateImage() {
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics g = image.getGraphics();
+		g.drawLine(0, 0, WIDTH, HEIGHT);
+		imageExpired = false;
+		return image;
 	}
 }
--- src/diagram/DiagramDisplay.java
+++ src/diagram/DiagramDisplay.java
@@ -14,14 +14,15 @@ public class DiagramDisplay {
 	
 	public void show() {
 		JFrame frame = new JFrame(diagram.toString());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		@SuppressWarnings("serial")
 		JPanel panel = new JPanel() {
 			@Override
 			public void paint(Graphics g) {
-				g.drawImage(diagram.createImage(), 0, 0, null);
+				g.drawImage(diagram.getImage(), 0, 0, null);
 			}
 		};
-		
-		
+		frame.add(panel);
+		frame.setVisible(true);
 	}
-	
 }
