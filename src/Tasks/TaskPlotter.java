13d7c1689f9bea7ab2eb0580ba7b3fef0c53cd83
zionzept <zionzept@gmail.com>
2016-03-06 18:58:31 +0100

    yee

--- src/Tasks/TaskPlotter.java
+++ src/Tasks/TaskPlotter.java
@@ -10,20 +10,21 @@ public class TaskPlotter {
 	
 	public TaskPlotter(){
 		LinkedList<Task> tasks = new LinkedList<Task>();
-//		Task t1a = new Task1a(1,90,1);
-//		tasks.add(t1a);
-//		
-//		Task t1b = new Task1b(1,90,1);
-//		tasks.add(t1b);
 		
-//		Task t2a = new Task2a(0,0,1*Math.pow(10, -2));
-//		tasks.add(t2a);
+		Task t1a = new Task1a(1,90,1);
+		tasks.add(t1a);
+		
+		Task t1b = new Task1b(1,90,1);
+		tasks.add(t1b);
+		
+		Task t2a = new Task2a(0,0,1*Math.pow(10, -2));
+		tasks.add(t2a);
 		
 		Task2b t2b = new Task2b(400 *Math.pow(10,-3),  700 *Math.pow(10,-3), 10 *Math.pow(10,-3));
 		tasks.add(t2b);
-//		
-//		Task2c t2c = new Task2c(400 *Math.pow(10,-3),  700 *Math.pow(10,-3), 10 *Math.pow(10,-3));
-//		tasks.add(t2c);
+		
+		Task2c t2c = new Task2c(400 *Math.pow(10,-3),  700 *Math.pow(10,-3), 10 *Math.pow(10,-3));
+		tasks.add(t2c);
 		
 		for(Task t: tasks){
 			LinkedList<Plot> plots = t.getPlots();
